0cda5666db36ee7c34036dac6248cfb64afa879c
root <root@609717.cloud4box.ru>
2023-12-19 20:21:01 +0300

    19.12

--- Kernighan_Ritchie/chapter_3_exs/ex_3.2.c
+++ Kernighan_Ritchie/chapter_3_exs/ex_3.2.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
-char escape(char s[], char t[]);
+#include <string.h>
+#define MAX 1000
+void escape(char s[], char t[]);
 
 int main(void)
 {
-	char mas_1[100];
-	char mas_2[200];
-
-	scanf("%s", mas);
-
-
-	printf ("%s", mas);
-	return 0;
+	int i;
+	char mass_1[MAX];
+	char  mass_2[MAX];
+	printf ("Введите первый массив\n");
+	for (i = 0; (mass_1[i] = getchar()) != EOF; i++) {}
+//    	mass_1[i] = '\0';
+   	escape (mass_1, mass_2);
+   	printf ("\nВторой массив:\n");
+ 	for (i = 0; i < strlen(mass_2); i++) 
+        	printf ("%c", mass_2[i]);
+    	printf ("\n");
+   	return 0;
 }
 
-char escape(char s[], char t[])
+void escape(char s[], char t[])
 {
-
-	for(int i = 0; i < strlen(s); i++)
-	{ 
-		switch (t[i])
+	int i;
+	for (i = 0; i < strlen(s); i++)
+	{
+		switch(s[i])
 		{
-			case
-
+			case '\t' : 
+				t[i] = '\\';
+				t[i+1] = 't';
+				i++;
+				break;
+			case '\n' :
+				t[i] = '\\';
+				t[i+1] = 'n';
+				i++;
+				break;
+			default :
+				t[i] = s[i];
+				break;	
+		}
+	}
+}
