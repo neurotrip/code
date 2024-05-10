26e1162dcc83997f5d47af43e7e239fd6238c974
root <root@609717.cloud4box.ru>
2024-05-10 15:15:31 +0300

    10.05

---
+++ Kerrisk_Linux/examples_chapter_55/example_55.5/is_seqnum.h
@@ -0,0 +1,10 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include "/home/code/Kerrisk_Linux/lib/read_line.h"
+#include "/home/code/Kerrisk_Linux/lib/tlpi_hdr.h"
+
+#define PORT_NUM "50000"
+
+#define INT_LEN 30
+
